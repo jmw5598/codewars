b817197c4215f2be7180e1ef028d1f8531d0bad9
Jason White <jmw5598@gmail.com>
2018-08-30 12:41:55 -0400

    5kyu Javascript : HashtagGenerator

---
+++ javascript/5kyu/TheHashtagGenerator.js
@@ -0,0 +1,28 @@
+/*
+  The marketing team is spending way too much time typing in hashtags.
+  Let's help them with out own Hashtag Generator!
+
+  Here's the deal:
+
+  It must start with a hashtag (#).
+    - All words must have their first letter capitalized.
+    - If the final result is longer than 140 chars it must return false.
+    - If the input or the result is an empty string it must return false.
+
+  Examples
+    " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+    "    Hello     World   "                  =>  "#HelloWorld"
+    ""                                        =>  false
+*/
+
+function generateHashtag(str) {
+  if(!str.trim()) return false;
+  let result = "#" + str.trim().split(/[\s]{1,}/g)
+     .map(capitalizeWord)
+     .join("");
+  return result.length > 140 ? false : result;
+}
+
+function capitalizeWord(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
